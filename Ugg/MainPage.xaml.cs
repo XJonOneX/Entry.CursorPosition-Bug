a8b9c5f55f5fe96a1cc12c6e6a0f399cafe7bcbb
Jon Harmon <JonDenverCO@gmail.com>
2022-11-20 11:24:49 -0500

    Add project files.

---
+++ Ugg/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Ugg;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		entryText.CursorPosition = 0;
+	}
+
+	private void entryText_Focused(object sender, FocusEventArgs e)
+	{
+		entryText.CursorPosition = 0;
+	}
+}
+
